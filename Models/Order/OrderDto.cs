ff9baa179598440397da27003a793dab27456515
GO672 <109583504+GO672@users.noreply.github.com>
2025-01-14 22:49:28 +0700

    Add files via upload

---
+++ Models/Order/OrderDto.cs
@@ -0,0 +1,31 @@
+﻿using Delivery_API.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class OrderDto
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    public DateTime DeliveryTime { get; set; }
+
+    [Required]
+    public DateTime OrderTime { get; set; }
+
+    [Required]
+    public OrderStatus Status { get; set; }
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public double Price { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string Address { get; set; }
+
+    // Link to user
+    [Required]
+    public string UserId { get; set; }
+
+    // Navigation Property
+    public List<DishOrder> Dishes { get; set; } = new List<DishOrder>();
+}
