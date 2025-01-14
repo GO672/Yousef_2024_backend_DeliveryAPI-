4840a812c8590f9d7b69180927615270b70c61b7
GO672 <109583504+GO672@users.noreply.github.com>
2025-01-14 22:50:29 +0700

    Create User.cs

---
+++ Models/User/User.cs
@@ -0,0 +1,17 @@
+using Delivery_API.Models.Enums;
+using System;
+
+namespace Delivery_API.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string PasswordHash { get; set; }  // Store password hash
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public Gender Gender { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
