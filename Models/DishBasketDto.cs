79ff49df80c54b9f0e8a3cbfeb69f10952b70a8a
GO672 <109583504+GO672@users.noreply.github.com>
2025-01-14 22:43:32 +0700

    Add files via upload

---
+++ Models/DishBasketDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class DishBasketDto
+{
+    [Key]
+    public Guid Id { get; set; } // Unique identifier for the dish
+
+    [Required]
+    [MinLength(1)]
+    public string Name { get; set; } // Name of the dish
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public double Price { get; set; } // Price per unit of the dish
+
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public double TotalPrice { get; set; } // Total cost (price × amount)
+
+    [Required]
+    [Range(1, int.MaxValue)]
+    public int Amount { get; set; } // Quantity of the dish
+
+    public string Image { get; set; } // Optional URL for the dish image
+
+    [Required]
+    public string UserId { get; set; } // ID of the user to whom the basket item belongs
+}
