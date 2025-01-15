b4275c4690234d7f5c82450fa50150acff12a97c
GO672 <109583504+GO672@users.noreply.github.com>
2025-01-15 22:47:24 +0700

    Add files via upload

---
+++ migrations/20241222115504_AddUserIdToBasket.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delivery_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToBasket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Basket",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Basket");
+        }
+    }
+}
---
+++ migrations/20241225105303_Dish.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Delivery_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Dish : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Rating",
+                table: "DishOrders",
+                type: "double precision",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "DishOrders");
+        }
+    }
+}
